5544d4dff2187337c7dae1e32faca53feaab599b
Jon Selzer <jselzer626@gmail.com>
2020-07-27 12:19:58 -0400

    added square function

--- src/index.js
+++ src/index.js
@@ -2,28 +2,39 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 
-function square() {
-
+function Square({index, markSquare}) {
+    return (
+        <button onClick={markSquare(index)}>
+            {index}
+        </button>
+    )
 }
 
-function board({ markSquare }) {
 
-    const [redIsNext, setNextColor] = useState(true)
-
-    const handleClick = () => {
-        e.preventDefault()
-        if
-    }
-}
 
 
 function App() {
 
-    const [gameBoard, setGameBoard] = useState(Array(49).fill(null))
-
-    const markSquare = (index, color) => {
+    const [gameBoard, setGameBoard] = useState(Array(16).fill(null))
+    const [redIsNext, setRedIsNext] = useState(true)
+    
+    const markSquare = (index) => {
         const newGameBoard = [... gameBoard]
-        newGameBoard[index].color = color
+        newGameBoard[index].color = redIsNext ? "red" : "black"
+        setRedIsNext(!redIsNext)
     }
+
+    return (
+        <div className="gameBoard">
+            {gameBoard.map((square, index) => (
+                <Square
+                    key={index}
+                    index={index}
+                    markSquare={markSquare}
+                />
+            ))} 
+        </div>
+
+    )
 
 }
