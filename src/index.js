ed223e09c3bba1a89f6d429bfb829c02fe1c7060
Jon Selzer <jselzer626@gmail.com>
2020-07-26 22:37:00 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function square() {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+}
+
+function game() {
+
+}
+
+
+function App() {
+
+    const [gameBoard, setGameBoard] = useState(Array(49).fill(null))
+
+
+    const markSquare = index => 
+
+
+}
