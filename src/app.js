063e38c277f6013f0a38e799dd2a3732f62b2dc7
Jon Selzer <jselzer626@gmail.com>
2020-07-30 21:55:46 -0400

    user input capture added

--- src/app.js
+++ src/app.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
+require('dotenv').config()
+
 const InputForm = (props) => (
 
-
-
-
-  
-        <form className={'ui form'} onSubmit={handleSubmit}>
+    <form className={'ui form'} onSubmit={handleSubmit}>
 
         <div className={'field'}>
             <label>What are you in the mood for?</label>
@@ -15,7 +13,7 @@ const InputForm = (props) => (
                 type='text' 
                 name='recipe_keywords' 
                 required 
-                onChange={e => setValue(e.target.value)}
+                onChange={props.handleInputChange}
                 placeholder="Search keywords, ingredients, cuisines etc.">
                 
             </input>    
@@ -28,17 +26,55 @@ const InputForm = (props) => (
 
 function App() {
 
-    const [keywords, setKeywords] = useState({})
-    const [results, setResults] = useState({})
+    const [input, setInput] = useState('')
+    const [results, setResults] = useState([])
+    const [resultIndex, setResultIndex] = useState(0)
+
+    const handleInputChange = e => {
+        newInput = input
+        newInput += e.currentTarget.value
+        setInput(newInput)
+    }
 
     const searchKeywords = async (e) => {
         e.preventDefault()
 
         try {
-            let pin = in
+            let keywords = input
+            if (!keywords)
+                return 
+            setInput('')
+            let fetchResults = await fetch(`https://api.spoonacular.com/recipes/search?query=${keywords}&
+            informationinstructionsRequired=true&${process.env.spoonacular_key}`)
+            let resultsJson = await fetchResults.json()
+
+            if (resultsJson.length === 0)
+            return
+        
+            setResults(resultsJson.results)
+
+        } 
+        catch(e) {
+            console.warn(e)
         }
 
+    }
 
+    const renderRecipe = () => {
+
+        let currentRecipe = results[resultIndex] ? results[resultIndex] : null
+
+        if (!currentRecipe)
+            return
+        
+        return (
+
+            <div>
+                
+            </div>
+
+        )
+        
     }
 
 
