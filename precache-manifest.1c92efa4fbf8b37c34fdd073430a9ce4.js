dd7c610073d21ad603b56430605f392dc7c03266
Jon Selzer <jselzer626@gmail.com>
2020-08-09 19:13:48 -0400

    Updates

---
+++ precache-manifest.1c92efa4fbf8b37c34fdd073430a9ce4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e4a80e4a8b934537c9bc983f18cfa1b",
+    "url": "/Dinner_TN_client/index.html"
+  },
+  {
+    "revision": "7e81ab6cd53169b26d6c",
+    "url": "/Dinner_TN_client/static/css/main.a87e5e52.chunk.css"
+  },
+  {
+    "revision": "ae9f05d830db57191e3a",
+    "url": "/Dinner_TN_client/static/js/2.aab62b93.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/Dinner_TN_client/static/js/2.aab62b93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e81ab6cd53169b26d6c",
+    "url": "/Dinner_TN_client/static/js/main.66fb1399.chunk.js"
+  },
+  {
+    "revision": "da677cff6051494b24fe",
+    "url": "/Dinner_TN_client/static/js/runtime-main.badb6503.js"
+  },
+  {
+    "revision": "b794f75dd9403b7d150e59fa52b04019",
+    "url": "/Dinner_TN_client/static/media/burger.b794f75d.png"
+  },
+  {
+    "revision": "9ce318dd8325322764950f7f8e2c28a4",
+    "url": "/Dinner_TN_client/static/media/crying.9ce318dd.png"
+  },
+  {
+    "revision": "286c336278e2adcb89964856d876c2c2",
+    "url": "/Dinner_TN_client/static/media/turkey.286c3362.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Dinner_TN_client/precache-manifest.1c92efa4fbf8b37c34fdd073430a9ce4.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Dinner_TN_client/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
